14e2733450c419419deb93ab1acf2c0af5704aa2
Nader <nader_abdelghani@hotmail.com>
2021-04-22 11:49:11 +0200

    Add a Temporary Test Bench Code Block in Core Module

--- core/core.py
+++ core/core.py
@@ -1,2 +1,17 @@
+import pandas as pd
+from transformers import AutoTokenizer
+
+from preprocessing import ConversationDataset
+from utilities.config import TOKENIZER_NAME, CACHE_DIR
+
+
 def get_response(user_utterance):
     return "Hey!"
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('../output/final_es_conv.csv')
+    df = df.dropna()
+    tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_NAME, cache_dir=CACHE_DIR)
+    dataset = ConversationDataset(tokenizer, df)
+    x = 5
