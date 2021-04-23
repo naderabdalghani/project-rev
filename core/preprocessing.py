b92a00d0e18f1f398f9b0e0de7ac6e0e4c184f43
mmmacmp <mmmacmp@gmail.com>
2021-04-23 23:11:45 +0200

    Implement Preprocessing to Work With The Downloaded Dataset

--- core/preprocessing.py
+++ core/preprocessing.py
@@ -1,36 +1,30 @@
 import os
 import pickle
 import torch
-from torch.utils.data import DataLoader, Dataset
-
-from utilities.config import CACHE_DIR, MODEL_TYPE, OVERWRITE_CACHE
-
-
-def construct_dialogue(row, tokenizer):
-    flatten = lambda l: [item for sublist in l for item in sublist]
-    conv = list(reversed([tokenizer.encode(x) + [tokenizer.eos_token_id] for x in row]))
-    conv = flatten(conv)
-    return conv
+from torch.utils.data import Dataset
+from utilities.config import CACHE_DIR, MODEL_TYPE, OVERWRITE_CACHE, DATASET_FILENAME, OUTPUT_DIR, DIALOGUE_SIZE
 
 
 class ConversationDataset(Dataset):
-    def __init__(self, tokenizer, df):
+    def __init__(self, tokenizer):
         block_size = tokenizer.model_max_length
         cached_features_file = os.path.join(CACHE_DIR, MODEL_TYPE + "_cached_lm_" + str(block_size))
         if os.path.exists(cached_features_file) and not OVERWRITE_CACHE:
-            print("Loading features from cached file %s", cached_features_file)
+            print("Loading features from cached file {}".format(cached_features_file))
             with open(cached_features_file, "rb") as cached_file:
                 self.dialogues = pickle.load(cached_file)
         else:
-            print("Creating features from dataset file at %s", CACHE_DIR)
+            print("Creating features from dataset file at {}".format(CACHE_DIR))
             self.dialogues = []
-            for _, row in df.iterrows():
-                dialogue = construct_dialogue(row, tokenizer)
-                if len(dialogue) > block_size:
-                    continue
-                self.dialogues.append(dialogue)
-
-            print("Saving features into cached file %s", cached_features_file)
+            with open(os.path.join(OUTPUT_DIR, DATASET_FILENAME)) as f:
+                file_content = f.readlines()
+                for i in range(0, len(file_content), DIALOGUE_SIZE):
+                    dialogue = file_content[i:min(i+DIALOGUE_SIZE, len(file_content))]
+                    tokenized_dialogue = [tokenizer.encode(line) + [tokenizer.eos_token_id] for line in dialogue]
+                    if len(tokenized_dialogue) > block_size:
+                        continue
+                    self.dialogues.append([token for line in tokenized_dialogue for token in line])
+            print("Saving features into cached file {}".format(cached_features_file))
             with open(cached_features_file, "wb") as handle:
                 pickle.dump(self.dialogues, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
