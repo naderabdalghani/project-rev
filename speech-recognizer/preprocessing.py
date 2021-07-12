e17dc9abd6a76013cb2b15b89c2989cba2756134
Muhanad <muhanad.atef23@gmail.com>
2021-07-12 22:24:25 +0200

    get_batch

--- speech-recognizer/preprocessing.py
+++ speech-recognizer/preprocessing.py
@@ -13,15 +13,14 @@ RNG_SEED = 123
 
 class AudioGenerator:
     def __init__(self, step=10, window=20, max_freq=8000, mfcc_dim=13,
-                 minibatch_size=20, desc_file=None, spectrogram=True, max_duration=10.0,
+                 minimum_batch_size=20, desc_file=None, spectrogram=True, max_duration=10.0,
                  sort_by_duration=False):
-        """
-        Params:
-            step (int): Step size in milliseconds between windows (for spectrogram ONLY)
-            window (int): FFT window size in milliseconds (for spectrogram ONLY)
-            max_freq (int): Only FFT bins corresponding to frequencies between
+        """ Generates training, validation and testing data
+            :param step: (int) Step size in milliseconds between windows (for spectrogram ONLY)
+            :param window: (int) FFT window size in milliseconds (for spectrogram ONLY)
+            :param max_freq: (int) Only FFT bins corresponding to frequencies between
                 [0, max_freq] are returned (for spectrogram ONLY)
-            desc_file (str, optional): Path to a JSON-line file that contains
+            :param desc_file: (str, optional) Path to a JSON-line file that contains
                 labels and paths to the audio files. If this is None, then
                 load metadata right away
         """
@@ -40,6 +39,64 @@ class AudioGenerator:
         self.cur_valid_index = 0
         self.cur_test_index = 0
         self.max_duration = max_duration
-        self.minibatch_size = minibatch_size
+        self.minimum_batch_size = minimum_batch_size
         self.spectrogram = spectrogram
         self.sort_by_duration = sort_by_duration
+
+    def get_batch(self, partition):
+        """ Obtain a batch of train, validation, or test data
+            :param partition: (string) Chooses from ('train', 'valid', 'test')
+            :raises: Exception if partition has a value different from ('train', 'valid', 'test')
+            :returns inputs: Contains the wav audio, label, input length and label length
+        """
+        if partition == 'train':
+            audio_paths = self.train_audio_paths
+            cur_index = self.cur_train_index
+            texts = self.train_texts
+        elif partition == 'valid':
+            audio_paths = self.valid_audio_paths
+            cur_index = self.cur_valid_index
+            texts = self.valid_texts
+        elif partition == 'test':
+            audio_paths = self.test_audio_paths
+            cur_index = self.test_valid_index
+            texts = self.test_texts
+        else:
+            raise Exception("Invalid partition. "
+                            "Must be train/validation or test")
+
+        features = [self.normalize(self.featurize(a)) for a in
+                    audio_paths[cur_index:cur_index + self.minimum_batch_size]]
+
+        # Calculate necessary sizes
+        max_length = max([features[i].shape[0]
+                          for i in range(0, self.minimum_batch_size)])
+        max_string_length = max([len(texts[cur_index + i])
+                                 for i in range(0, self.minimum_batch_size)])
+
+        # Initialize the arrays
+        input_data = np.zeros([self.minimum_batch_size, max_length,
+                               self.feat_dim * self.spectrogram + self.mfcc_dim * (not self.spectrogram)])
+        labels = np.ones([self.minimum_batch_size, max_string_length]) * 28  # Set all labels as blank
+        input_length = np.zeros([self.minimum_batch_size, 1])
+        label_length = np.zeros([self.minimum_batch_size, 1])
+
+        for i in range(0, self.minimum_batch_size):
+            # Calculate input_data & input_length
+            feat = features[i]
+            input_length[i] = feat.shape[0]
+            input_data[i, :feat.shape[0], :] = feat
+
+            # Calculate labels & label_length
+            label = np.array(text_to_int_sequence(texts[cur_index + i]))
+            labels[i, :len(label)] = label
+            label_length[i] = len(label)
+
+        # Return the arrays
+        outputs = {'ctc': np.zeros([self.minimum_batch_size])}
+        inputs = {'the_input': input_data,
+                  'the_labels': labels,
+                  'input_length': input_length,
+                  'label_length': label_length
+                  }
+        return inputs, outputs
