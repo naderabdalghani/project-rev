38698ea363957b29388dcefcb2eb476a46aa93dd
mmmacmp <mmmacmp@gmail.com>
2021-07-21 19:59:50 +0200

    Build and preprocess N-gram model

--- language-model/build_dictionary.py
+++ language-model/build_dictionary.py
@@ -1,83 +1,269 @@
+import pickle
+import nltk
+import os
+from utilities.config import OUTPUT_DIR
 
-from utils import load_file
-from collections import Counter
-import json
-
-
-def export_word_frequency(filepath, word_frequency):
-    """ Generate a json object from a word frequency
-        Args:
-            filepath (str):
-            word_frequency (Counter):
+def split_to_sentences(data):
     """
-    with open(filepath, 'w') as f:
-        json.dump(word_frequency, f, indent="", sort_keys=True, ensure_ascii=False)
+    Split data by linebreak "\n"
 
+    Args:
+        data: str
 
-def is_english(s):
-    try:
-        s.encode(encoding='utf-8').decode('ascii')
-    except UnicodeDecodeError:
-        return False
-    else:
-        return True
-
-
-def build_word_frequency(filepath, output_path):
-    """ Parse the passed text file (from Open Subtitles) into
-        a word frequency list and save it to disk
-        Args:
-            filepath (str):
-            output_path (str):
-        Returns:
-            Counter: The word frequency as parsed from the file
-        Note:
-            This only removes words that are proper nouns (attempts to...) and
-            anything that starts or stops with something that is not in the alphabet.
+    Returns:
+        A list of sentences
     """
-    # NLTK is only needed in this portion of the project
-    try:
-        from nltk.tag import pos_tag
-        from nltk.tokenize import WhitespaceTokenizer
-    except ImportError as ex:
-        raise ImportError("To build a dictionary from scratch, NLTK is required!\n{}".format(ex.message))
+    sentences = data.split("\n")
 
-    word_frequency = Counter()
-    tok = WhitespaceTokenizer()
-    idx = 0
-    vowels = set("aeiouy")
+    # Additional clearning (This part is already implemented)
+    # - Remove leading and trailing spaces from each sentence
+    # - Drop sentences if they are empty strings.
+    sentences = [s.strip() for s in sentences]
+    sentences = [s for s in sentences if len(s) > 0]
 
-    # num_lines = sum(1 for line in open(filepath))
-    # num_lines = 441450449
-    with load_file(filepath) as fobj:
-        for line in fobj:
-            # print(line)
-            line = line.lower()
+    return sentences
 
-            # tokenize into parts
-            parts = tok.tokenize(line)
 
-            # Attempt to remove proper nouns
-            tagged_sent = pos_tag(parts)
+def tokenize_sentences(sentences):
+    """
+    Tokenize sentences into tokens (words)
 
-            # Remove words with invalid characters
-            # Remove things that have leading or trailing non-alphabetic characters.
-            # Remove words without a vowel
-            # Remove ellipses
-            # Remove Double punctuations
-            words = [word[0] for word in tagged_sent if word[0] and is_english(word[0]) and not word[1] == "NNP" and word[0].isalnum() and not vowels.isdisjoint(word[0]) and not word[0].count("'") > 1 and not word[0].count(".") > 2 and not ".." in word[0] and word[0][0].isalpha() and word[0][-1].isalpha()]
+    Args:
+        sentences: List of strings
 
-            if words:
-                # print(words)
-                word_frequency.update(words)
+    Returns:
+        List of lists of tokens
+    """
 
-            idx += 1
+    # Initialize the list of lists of tokenized sentences
+    tokenized_sentences = []
 
-            if idx % 100000 == 0:
-                print("completed: {} rows".format(idx))
+    # Go through each sentence
+    for sentence in sentences:
+        # Convert to lowercase letters
+        sentence = sentence.lower()
 
-    print("completed: {} rows".format(idx))
+        # Convert into a list of words
+        tokenized = nltk.word_tokenize(sentence)
 
-    export_word_frequency(output_path, word_frequency)
+        # append the list of words to the list of lists
+        tokenized_sentences.append(tokenized)
 
-    return word_frequency
+    return tokenized_sentences
+
+
+def get_tokenized_data(data):
+    """
+    Make a list of tokenized sentences
+
+    Args:
+        data: String
+
+    Returns:
+        List of lists of tokens
+    """
+
+    # Get the sentences by splitting up the data
+    sentences = split_to_sentences(data)
+    print(len(sentences))
+    # Get the list of lists of tokens by tokenizing the sentences
+    tokenized_sentences = tokenize_sentences(sentences)
+
+    return tokenized_sentences
+
+
+def count_words(tokenized_sentences):
+    """
+    Count the number of word appearence in the tokenized sentences
+
+    Args:
+        tokenized_sentences: List of lists of strings
+
+    Returns:
+        dict that maps word (str) to the frequency (int)
+    """
+
+    word_counts = {}
+
+    # Loop through each sentence
+    for sentence in tokenized_sentences:  # complete this line
+
+        # Go through each token in the sentence
+        for token in sentence:  # complete this line
+
+            # If the token is not in the dictionary yet, set the count to 1
+            if token not in word_counts.keys():  # complete this line
+                word_counts[token] = 1
+
+            # If the token is already in the dictionary, increment the count by 1
+            else:
+                word_counts[token] += 1
+
+    return word_counts
+
+
+def get_words_with_nplus_frequency(tokenized_sentences, count_threshold):
+    """
+    Find the words that appear N times or more
+
+    Args:
+        tokenized_sentences: List of lists of sentences
+        count_threshold: minimum number of occurrences for a word to be in the closed vocabulary.
+
+    Returns:
+        List of words that appear N times or more
+    """
+    # Initialize an empty list to contain the words that
+    # appear at least 'minimum_freq' times.
+    closed_vocab = []
+
+    # Get the word couts of the tokenized sentences
+    # Use the function that you defined earlier to count the words
+    word_counts = count_words(tokenized_sentences)
+
+    # for each word and its count
+    for word, cnt in word_counts.items():  # complete this line
+
+        # check that the word's count
+        # is at least as great as the minimum count
+        if cnt >= count_threshold:
+            # append the word to the list
+            closed_vocab.append(word)
+
+    return closed_vocab
+
+
+def replace_oov_words_by_unk(tokenized_sentences, vocabulary, unknown_token="<unk>"):
+    """
+    Replace words not in the given vocabulary with '<unk>' token.
+
+    Args:
+        tokenized_sentences: List of lists of strings
+        vocabulary: List of strings that we will use
+        unknown_token: A string representing unknown (out-of-vocabulary) words
+
+    Returns:
+        List of lists of strings, with words not in the vocabulary replaced
+    """
+
+    # Place vocabulary into a set for faster search
+    vocabulary = set(vocabulary)
+
+    # Initialize a list that will hold the sentences
+    # after less frequent words are replaced by the unknown token
+    replaced_tokenized_sentences = []
+
+    # Go through each sentence
+    for sentence in tokenized_sentences:
+
+        # Initialize the list that will contain
+        # a single sentence with "unknown_token" replacements
+        replaced_sentence = []
+
+        # for each token in the sentence
+        for token in sentence:  # complete this line
+
+            # Check if the token is in the closed vocabulary
+            if token in vocabulary:  # complete this line
+                # If so, append the word to the replaced_sentence
+                replaced_sentence.append(token)
+            else:
+                # otherwise, append the unknown token instead
+                replaced_sentence.append("<unk>")
+
+        # Append the list of tokens to the list of lists
+        replaced_tokenized_sentences.append(replaced_sentence)
+
+    return replaced_tokenized_sentences
+
+
+def preprocess_data(train_data, count_threshold):
+    """
+    Preprocess data, i.e.,
+        - Find tokens that appear at least N times in the training data.
+        - Replace tokens that appear less than N times by "<unk>" both for training and test data.
+    Args:
+        train_data, test_data: List of lists of strings.
+        count_threshold: Words whose count is less than this are
+                      treated as unknown.
+
+    Returns:
+        Tuple of
+        - training data with low frequent words replaced by "<unk>"
+        - test data with low frequent words replaced by "<unk>"
+        - vocabulary of words that appear n times or more in the training data
+    """
+
+    # Get the closed vocabulary using the train data
+    vocabulary = get_words_with_nplus_frequency(train_data, count_threshold)
+
+    # For the train data, replace less common words with "<unk>"
+    train_data_replaced = replace_oov_words_by_unk(train_data, vocabulary)
+
+    return train_data_replaced, vocabulary
+
+
+def count_n_grams(data, n, start_token='<s>', end_token='<e>'):
+    """
+    Count all n-grams in the data
+
+    Args:
+        data: List of lists of words
+        n: number of words in a sequence
+
+    Returns:
+        A dictionary that maps a tuple of n-words to its frequency
+    """
+
+    # Initialize dictionary of n-grams and their counts
+    n_grams = {}
+
+    # Go through each sentence in the data
+    for sentence in data:  # complete this line
+
+        # prepend start token n-1 times, and  append <e> one time
+        sentence = [start_token] * (n-1) + sentence + [end_token]
+        # convert list to tuple
+        # So that the sequence of words can be used as
+        # a key in the dictionary
+        sentence = tuple(sentence)
+
+        # Use 'i' to indicate the start of the n-gram
+        # from index 0
+        # to the last index where the end of the n-gram
+        # is within the sentence.
+
+        m = len(sentence) if n == 1 else len(sentence) - 1
+        for i in range(m):  # complete this line
+
+            # Get the n-gram from i to i+n
+            n_gram = sentence[i:i + n]
+
+            # check if the n-gram is in the dictionary
+            if n_gram in n_grams.keys():  # complete this line
+
+                # Increment the count for this n-gram
+                n_grams[n_gram] += 1
+            else:
+                # Initialize this n-gram count to 1
+                n_grams[n_gram] = 1
+
+    return n_grams
+
+
+if __name__ == '__main__':
+    with open(os.path.join(OUTPUT_DIR, "en_US.twitter.txt"), encoding='utf-8') as f:
+        data = f.read()
+    print("Data type:", type(data))
+    print("Number of letters:", len(data))
+    train_data = get_tokenized_data(data)
+    minimum_freq = 2
+    train_data_processed, vocabulary = preprocess_data(train_data, minimum_freq)
+    unigrams = count_n_grams(train_data_processed, 1)
+    bigrams = count_n_grams(train_data_processed, 2)
+    trigrams = count_n_grams(train_data_processed, 3)
+    print(len(unigrams),len(vocabulary))
+    pickle.dump(unigrams, open(os.path.join(OUTPUT_DIR, "unigrams_tuples"), 'wb'))
+    pickle.dump(bigrams, open(os.path.join(OUTPUT_DIR, "bigrams_tuples"), 'wb'))
+    pickle.dump(trigrams, open(os.path.join(OUTPUT_DIR, "trigrams_tuples"), 'wb'))
+    print ("Dictionaries Built Successfully")
