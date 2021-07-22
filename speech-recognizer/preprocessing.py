a586d2907077b87cf87c7205e16475e7ad3a0ea7
Muhanad <muhanad.atef23@gmail.com>
2021-07-22 04:35:39 +0200

    Preprocess function

--- speech-recognizer/preprocessing.py
+++ speech-recognizer/preprocessing.py
@@ -1,7 +1,8 @@
+import torch
 import torchaudio
 import torch.nn as nn
 
-from config import FEATURE_USED, SAMPLING_RATE
+from config import FEATURE_USED, SAMPLING_RATE, TEXT_TRANSFORMER
 
 train_audio_transforms = nn.Sequential(
     torchaudio.transforms.MelSpectrogram(sample_rate=SAMPLING_RATE, n_mels=FEATURE_USED['n_features'])
@@ -17,3 +18,29 @@ valid_audio_transforms = torchaudio.transforms.MelSpectrogram(sample_rate=SAMPLI
                                                                                  n_mfcc=FEATURE_USED['n_features'])
 
 
+def preprocess(data, data_type="train"):
+    """
+    This function do the preprocessing phase for raw audio data.
+    :param data: Audio files from the dataset
+    :param data_type: Train or validation
+    :return: spectrograms, labels, input_lengths, label_lengths
+    """
+    spectrograms = []
+    labels = []
+    input_lengths = []
+    label_lengths = []
+    for (waveform, _, utterance, _, _, _) in data:
+        if data_type == 'train':
+            spec = train_audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        else:
+            spec = valid_audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        spectrograms.append(spec)
+        label = torch.Tensor(TEXT_TRANSFORMER.text_to_int(utterance.lower()))
+        labels.append(label)
+        input_lengths.append(spec.shape[0] // 2)
+        label_lengths.append(len(label))
+
+    spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
+    labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
+
+    return spectrograms, labels, input_lengths, label_lengths
