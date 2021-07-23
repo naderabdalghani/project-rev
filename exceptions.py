83b97d12a420cc341afe05425128c9f53c45e9b4
Nader <nader_abdelghani@hotmail.com>
2021-07-23 19:50:22 +0200

    Add speech synthesizer not loaded exception

--- exceptions.py
+++ exceptions.py
@@ -14,3 +14,9 @@ class LanguageModelNotTrained(Exception):
     def __init__(self, message="No saved instance of the language model was found. "
                                "Please make sure the model is trained and saved."):
         super().__init__(message)
+
+
+class SpeechSynthesizerCannotBeLoaded(Exception):
+    def __init__(self, message="No saved instance of the synthesizer model was found. "
+                               "Please make sure there is a saved instance of the model"):
+        super().__init__(message)
