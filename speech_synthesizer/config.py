ba55ce4f53036732238e3aaa6070da876fd4ca49
Nader <nader_abdelghani@hotmail.com>
2021-08-16 21:49:22 +0200

    Move speech synthesizer voice sample to /models directory

--- speech_synthesizer/config.py
+++ speech_synthesizer/config.py
@@ -1,6 +1,6 @@
 import os
 
-from app_config import DATA_DIR
+from app_config import MODELS_DIR
 
-TED_VOICE_SAMPLE_PATH = os.path.join(DATA_DIR, "TED_VOICE_SAMPLE.wav")
+TED_VOICE_SAMPLE_PATH = os.path.join(MODELS_DIR, 'speech-synthesizer', "TED_VOICE_SAMPLE.wav")
 SAVED_INSTANCE_DIR = "speech-synthesizer"
